2069a7eb9e5e40297342c839f4db081e9b799f8a
Ferdy Arrahman Damin <ferdy.damin@ciputralife-local.co.id>
2020-07-23 18:05:33 +0700

    update

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-    <div class="container">
-      <img src="{{asset('assets/images/logo_login.png')}}">
+    <div class="container" style="margin-bottom:20px; margin-top:10px">
+      <img src="{{asset('assets/images/logo_login.png')}}" width="15%">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="oi oi-menu"></span> Menu
+        <span class="oi oi-menu"></span>gity 
       </button>
 
       <div class="collapse navbar-collapse" id="ftco-nav">
